7fdf79fd4749b156369e134439be98154a5918f8
Mehfooj <162735469+Savvythelegend@users.noreply.github.com>
2025-07-16 17:03:38 +0530

    Delete Python/Practice codes directory

--- Python/Practice codes/helper.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-logger = logging.getLogger(__name__) 
-logger.info('This is a log info')
-logger.debug('Debugging')
